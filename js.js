7b419ed2658148eeb365c60279e091c084389115
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-19 00:59:55 +0100

    1

--- js.js
+++ js.js
@@ -12,14 +12,18 @@ function myFunction() {
   document.getElementById("deemo").innerHTML = z;
   document.getElementById("demo").innerHTML = x;
   if(x.includes("+")){
-    var split = x.split("+")}
+    var split = x.split("+")
+  console.log(split[0])}
   else if(x.includes("-")){
-    var split = x.split("-")}
+    var split = x.split("-")
+    console.log(split[0])}
     else if(x.includes("/")){
-      var split = x.split("/")}
+      var split = x.split("/")
+    console.log(split[0])}
       else if(x.includes("*")){
-        var split = x.split("*")}}
-
+        var split = x.split("*")
+      console.log(split[0])}
+      }
 
 window.onload = myFunction()
 
