f8dd641075ec60e0a3861c1c6ea6c0e0fff95301
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-18 20:47:31 +0100

    test

--- js.js
+++ js.js
@@ -4,13 +4,11 @@ function myFunction() {
 
   document.getElementById("demo").innerHTML = x;}
 
-  // Execute a function when the user releases a key on the keyboard
- x.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
+var y = document.getElementById("help").value;
+
+ y.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("demo").innerHTML = x;
   }
 });
