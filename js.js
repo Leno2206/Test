e0d0de33312de5f5ea0d5fcff53bb49026162a7a
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-18 21:59:33 +0100

    123

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
 function myFunction() {
   var x = document.getElementById("help").value;
+  var z =""
   if(x ===""){
-    x="Please enter a value"
+    z="Please enter a value"
   }
   else if(x!=""){
     x=document.getElementById("help").value
   }
-  document.getElementById("demo").innerHTML = x;}
+  document.getElementById("demo").innerHTML = x;
+if(z!=""){
+  document.getElementById("deemo").innerHTML = z;
+}}
