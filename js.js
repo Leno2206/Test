d25d3ad4167d9d724666794ef784ad630698dcae
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-18 21:50:51 +0100

    js

--- js.js
+++ js.js
@@ -1,14 +1,10 @@
 function myFunction() {
   var x = document.getElementById("help").value;
-  console.log(x)
+  if(x ===""){
+    x="Please enter a value"
+  }
+  else if(x!=""){
+    x=document.getElementById("help").value
+  }
 
   document.getElementById("demo").innerHTML = x;}
-
-var y = document.getElementById("help").value;
-
- y.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("demo").innerHTML = x;
-  }
-});
