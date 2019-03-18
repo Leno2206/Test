07c323e49673afb45c76a43ef58fa4927255f2bb
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-18 19:18:36 +0100

    new server

--- js.js
+++ js.js
@@ -1,1 +1,5 @@
-document.getElementById("submit").value;
+function myFunction() {
+  var x = document.getElementById("help").value;
+  console.log(x)
+
+  document.getElementById("demo").innerHTML = x;}
