99532e4787bedf6a5bc0583f4faa480f8d4d6892
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-20 17:43:47 +0100

    .=_

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ window.onload = CaesarCipher()
  function CaesarCipher() {
    if(document.getElementById('Encode').checked){
       var str = document.getElementById("Input").value
-      var str.lower = String(str).toLowerCase();
+      var str_lower = String(str).toLowerCase();
       var error
      if(str===""){
        error ="Please enter a value"
@@ -15,7 +15,7 @@ window.onload = CaesarCipher()
      var charcode = 0;
 
      for (var i = 0; i < str.length; i++) {
-         charcode = (str.lower[i].charCodeAt()) + 2;
+         charcode = (str_lower[i].charCodeAt()) + 2;
          result += String.fromCharCode(charcode);
      }
      document.getElementById("Message").innerHTML = result;
@@ -23,7 +23,7 @@ window.onload = CaesarCipher()
 
 if(document.getElementById("Decode").checked){
    var str = document.getElementById("Input").value
-   var str.lower = String(str).toLowerCase();
+   var str_lower = String(str).toLowerCase();
    var error
   if(str===""){
     error ="Please enter a value"
@@ -35,7 +35,7 @@ if(document.getElementById("Decode").checked){
   var charcode = 0;
 
   for (var i = 0; i < str.length; i++) {
-      charcode = (str.lower[i].charCodeAt()) - 2;
+      charcode = (str_lower[i].charCodeAt()) - 2;
       result += String.fromCharCode(charcode);
   }
   document.getElementById("Message").innerHTML = result;
