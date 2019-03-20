28031d8153c2537e82f5ee6a824caa5b012c7006
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-20 17:02:22 +0100

    12311111

--- js.js
+++ js.js
@@ -32,7 +32,32 @@ window.onload = CaesarCipher()
          result += String.fromCharCode(charcode);
      }
      document.getElementById("demo").innerHTML = result;
+}}
+
+if(document.getElementById("Decode").checked="true"){
+   var str = document.getElementById("help").value
+   var virus = String(str).toLowerCase();
+   var amk
+  if(str===""){
+    amk ="Please enter a value"
+    document.getElementById("demo").style.color =  "red"
+    document.getElementById('demo').innerHTML=amk}
+    else if (str!=""){
+      document.getElementById("demo").style.color =  "green"
+  var result = '';
+  var charcode = 0;
+
+  for (var i = 0; i < str.length; i++) {
+      charcode = (virus[i].charCodeAt()) - 2;
+      result += String.fromCharCode(charcode);
+  }
+  document.getElementById("demo").innerHTML = result;
 }}}
+
+
+
+
+}
 function CaesarDecipher() {
   if(document.getElementById("Decode").checked="true"){
      var str = document.getElementById("help").value
