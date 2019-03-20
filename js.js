78cd004ff066df3a51de65e03a55a7be32b1e078
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-20 20:06:56 +0100

    no outline

--- js.js
+++ js.js
@@ -41,3 +41,12 @@ if(document.getElementById("Decode").checked){
   }
   document.getElementById("Message").value = result;
 }}}
+
+function resizable (el, factor) {
+  var int = Number(factor) || 7.7;
+  function resize() {el.style.width = ((el.value.length+1) * int) + 'px'}
+  var e = 'keyup,keypress,focus,blur,change'.split(',');
+  for (var i in e) el.addEventListener(e[i],resize,false);
+  resize();
+}
+resizable(document.getElementById('input'),7);
