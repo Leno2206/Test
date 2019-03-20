1499e73563e9ab5b96c9567d5f64336f2532bec4
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-20 17:42:06 +0100

    Beautify

--- js.js
+++ js.js
@@ -1,76 +1,42 @@
-/* function myFunction() {
-  var x = document.getElementById("help").value;
-  var z =""
-  if(x ===""){
-    z="Please enter a value"
-    document.getElementById("demo").innerHTML = z;
-  }
-  else if(x!=""){
-    x=document.getElementById("help").value
-    document.getElementById("demo").innerHTML = x;
-  }
-
-
-}*/
 window.onload = CaesarCipher()
  function CaesarCipher() {
    if(document.getElementById('Encode').checked){
-      var str = document.getElementById("help").value
-      var virus = String(str).toLowerCase();
-      var amk
+      var str = document.getElementById("Input").value
+      var str.lower = String(str).toLowerCase();
+      var error
      if(str===""){
-       amk ="Please enter a value"
-       document.getElementById("demo").style.color =  "red"
-       document.getElementById('demo').innerHTML=amk}
+       error ="Please enter a value"
+       document.getElementById("Message").style.color =  "red"
+       document.getElementById('Message').innerHTML=error}
        else if (str!=""){
-         document.getElementById("demo").style.color =  "green"
-         document.getElementById("help").value=""
+         document.getElementById("Message").style.color =  "green"
+         document.getElementById("Input").value=""
      var result = '';
      var charcode = 0;
 
      for (var i = 0; i < str.length; i++) {
-         charcode = (virus[i].charCodeAt()) + 2;
+         charcode = (str.lower[i].charCodeAt()) + 2;
          result += String.fromCharCode(charcode);
      }
-     document.getElementById("demo").innerHTML = result;
+     document.getElementById("Message").innerHTML = result;
 }}
 
 if(document.getElementById("Decode").checked){
-   var str = document.getElementById("help").value
-   var virus = String(str).toLowerCase();
-   var amk
+   var str = document.getElementById("Input").value
+   var str.lower = String(str).toLowerCase();
+   var error
   if(str===""){
-    amk ="Please enter a value"
-    document.getElementById("demo").style.color =  "red"
-    document.getElementById('demo').innerHTML=amk}
+    error ="Please enter a value"
+    document.getElementById("Message").style.color =  "red"
+    document.getElementById('Message').innerHTML=error}
     else if (str!=""){
-      document.getElementById("demo").style.color =  "green"
+      document.getElementById("Message").style.color =  "green"
   var result = '';
   var charcode = 0;
 
   for (var i = 0; i < str.length; i++) {
-      charcode = (virus[i].charCodeAt()) - 2;
+      charcode = (str.lower[i].charCodeAt()) - 2;
       result += String.fromCharCode(charcode);
   }
-  document.getElementById("demo").innerHTML = result;
+  document.getElementById("Message").innerHTML = result;
 }}}
-/*function CaesarDecipher() {
-  if(document.getElementById("Decode").checked="true"){
-     var str = document.getElementById("help").value
-     var virus = String(str).toLowerCase();
-     var amk
-    if(str===""){
-      amk ="Please enter a value"
-      document.getElementById("demo").style.color =  "red"
-      document.getElementById('demo').innerHTML=amk}
-      else if (str!=""){
-        document.getElementById("demo").style.color =  "green"
-    var result = '';
-    var charcode = 0;
-
-    for (var i = 0; i < str.length; i++) {
-        charcode = (virus[i].charCodeAt()) - 2;
-        result += String.fromCharCode(charcode);
-    }
-    document.getElementById("demo").innerHTML = result;
-}}}*/
