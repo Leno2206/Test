b77d2bd1cad2aebc3224cc19c78067b64e3951c3
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-23 23:47:15 +0100

    Beautify

--- js.js
+++ js.js
@@ -1,55 +1,59 @@
 window.onload = CaesarCipher()
- function CaesarCipher() {
-   if(document.getElementById('Encode').checked){
-      var str = document.getElementById("Input").value
-      var str_lower = String(str).toLowerCase();
-      var error
-      RadioEncode()
-     if(str===""){
-       error ="Please enter a value..."
-       document.getElementById("Message").style.color =  "red"
-       document.getElementById('Message').value=error}
-       else if (str!=""){
-         document.getElementById("Message").style.color =  "green"
-         document.getElementById("Input").value=""
-     var result = '';
-     var charcode = 0;
 
-     for (var i = 0; i < str.length; i++) {
-         charcode = (str_lower[i].charCodeAt()) + 2;
-         result += String.fromCharCode(charcode);
-     }
-     document.getElementById("Message").value = result;
-}}
+function CaesarCipher() {
+  if (document.getElementById('Encode').checked) {
+    var str = document.getElementById("Input").value
+    var str_lower = String(str).toLowerCase();
+    var error
+    RadioEncode()
+    if (str === "") {
+      error = "Please enter a value..."
+      document.getElementById("Message").style.color = "red"
+      document.getElementById('Message').value = error
+    } else if (str != "") {
+      document.getElementById("Message").style.color = "green"
+      document.getElementById("Input").value = ""
+      var result = '';
+      var charcode = 0;
 
-if(document.getElementById("Decode").checked){
-   var str = document.getElementById("Input").value
-   var str_lower = String(str).toLowerCase();
-   var error
-   RadioDecode()
-  if(str===""){
-    error ="Please enter a value..."
-    document.getElementById("Message").style.color =  "red"
-    document.getElementById('Message').value=error}
-    else if (str!=""){
-      document.getElementById("Message").style.color =  "green"
-      document.getElementById("Input").value=""
-  var result = '';
-  var charcode = 0;
-
-  for (var i = 0; i < str.length; i++) {
-      charcode = (str_lower[i].charCodeAt()) - 2;
-      result += String.fromCharCode(charcode);
+      for (var i = 0; i < str.length; i++) {
+        charcode = (str_lower[i].charCodeAt()) + 2;
+        result += String.fromCharCode(charcode);
+      }
+      document.getElementById("Message").value = result;
+    }
   }
-  document.getElementById("Message").value = result;
-}}}
 
-function ShowPassword(){
-  var InputPass= document.getElementById("Input");
-  if (InputPass.type==="password"){
-    InputPass.type= "text";
-  } else{
-    InputPass.type="password";
+  if (document.getElementById("Decode").checked) {
+    var str = document.getElementById("Input").value
+    var str_lower = String(str).toLowerCase();
+    var error
+    RadioDecode()
+    if (str === "") {
+      error = "Please enter a value..."
+      document.getElementById("Message").style.color = "red"
+      document.getElementById('Message').value = error
+    } else if (str != "") {
+      document.getElementById("Message").style.color = "green"
+      document.getElementById("Input").value = ""
+      var result = '';
+      var charcode = 0;
+
+      for (var i = 0; i < str.length; i++) {
+        charcode = (str_lower[i].charCodeAt()) - 2;
+        result += String.fromCharCode(charcode);
+      }
+      document.getElementById("Message").value = result;
+    }
+  }
+}
+
+function ShowPassword() {
+  var InputPass = document.getElementById("Input");
+  if (InputPass.type === "password") {
+    InputPass.type = "text";
+  } else {
+    InputPass.type = "password";
   }
 }
 
@@ -57,23 +61,24 @@ function ShowPassword(){
 var Enter = document.getElementById("Input");
 Enter.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("submit").click();
+    event.preventDefault();
+    document.getElementById("submit").click();
   }
 });
 
 
 
-function RadioEncode(){
-  document.getElementById("submit").innerHTML=
-  "ENCODE!"
+function RadioEncode() {
+  document.getElementById("submit").innerHTML =
+    "ENCODE!"
 }
-function RadioDecode(){
-  document.getElementById("submit").innerHTML=
-  "DECODE!"
+
+function RadioDecode() {
+  document.getElementById("submit").innerHTML =
+    "DECODE!"
 }
 
 
-function Roll(){
-  document.getElementById("Ball").className +=" BallAnim"
+function Roll() {
+  document.getElementById("Ball").className += " BallAnim"
 }
