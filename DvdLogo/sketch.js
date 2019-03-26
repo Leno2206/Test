6a19296a0f0fa9167ae0edba36953ec6eafbeec6
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-26 19:24:46 +0100

    1

--- DvdLogo/sketch.js
+++ DvdLogo/sketch.js
@@ -25,7 +25,7 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(0);
+  background(colorMode(HSB, 0, 0, 100));
   image(dvd, x, y, dvd.width / 2, dvd.height / 2)
   //rect(x, y, 80, 60);
   x = x + xspeed;
