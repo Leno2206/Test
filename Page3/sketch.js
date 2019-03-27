df06ecd720ec4e6fbc6c38287fc41eb006dff755
Leno2206 <35237256+Leno2206@users.noreply.github.com>
2019-03-27 21:21:53 +0100

    2

--- Page3/sketch.js
+++ Page3/sketch.js
@@ -43,6 +43,7 @@ function draw() {
   if (GameOver == 0) {
     Time = Time - Dif
     rect(RectX, RectY, RectW, RectH)
+    tint(random(0, 256), random(0, 256), random(0, 256), 100)
   }
   if (mouseIsPressed && GameOver <= 0) {
     ellipse(mouseX, mouseY, 100)
@@ -55,6 +56,7 @@ function draw() {
     points++
     document.getElementById("Points").value = points
     Dif = Dif * 1.01
+    tint(random(0, 256), random(0, 256), random(0, 256), 100)
 
     Time = 10
 
